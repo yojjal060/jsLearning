31d8831fa4bafc8d4a0d6ade39c79a716a2f00e3
Yojjal Sharma <yojjal878@gmail.com>
2024-05-31 02:09:30 +0545

    yes

---
+++ ecommerce-product-page/app.js
@@ -0,0 +1,35 @@
+let increments = document.getElementById("increment")
+let decrements = document.getElementById("decrement")
+let input = document.getElementById("addSubValue")
+let cartIcon = document.getElementById("cart-image")
+let cart = document.getElementById("whole-cart")
+
+
+function increment(){
+    if(Number(input.value)>-1){
+        input.value = Number(input.value)+1;
+    }
+}
+
+function decrement(){
+    if(Number(input.value)>0){
+        input.value = Number(input.value)-1;
+    }
+}
+
+function addToCart(){
+    if (Number(input.value) > 0){
+        console.log("added to cart")
+        
+    }
+    else{
+        console.log("Please add a quantity")
+    
+    }
+}
+
+function toggleCart(){
+    cart.classList.toggle("hidden");
+    console.log("cart")
+}
+
