fb376393c8fb38eab03e3966289861c14bddc7a6
Yojjal Sharma <yojjal878@gmail.com>
2024-06-12 13:52:22 +0545

    all changes for deployment

--- ecommerce-product-page/tailwind.config.js
+++ ecommerce-product-page/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'customorange': 'hsl(26, 100%, 55%)'
+      },
+      fontFamily: {
+        'poetsenone': ['Poetsen One', 'sans-serif']
+      },
+      width: (() => {
+        const widthScale = {};
+        for (let i = 1; i <= 100; i++) {
+          widthScale[i] = `${i * 0.25}rem`;
+        }
+        return widthScale;
+      })(),
+    },
   },
+  variants: {},
   plugins: [],
 }
-
