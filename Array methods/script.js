67a57db23a707e1cc62adb5b7e74b37675e1584b
Yojjal Sharma <yojjal878@gmail.com>
2024-07-04 23:01:05 +0545

    ok

---
+++ Array methods/script.js
@@ -0,0 +1,11 @@
+// let arr = ["I", "study", "JavaScript"];
+
+// arr.splice(0, 2,'lol','damn'); // from index 1 remove 1 element
+
+
+// console.log( arr ); // ["I", "JavaScript"]
+
+let arr = ["t", "e", "s", "t"];
+
+console.log( arr.slice(0, 2) ); // e,s (copy from 1 to 3)
+console.log( arr.slice(-1) );
